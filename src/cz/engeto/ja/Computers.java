22c618743eb459a71fb2563beae1489686c6d1aa
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-02-14 10:19:01 +0100

    Vytvořila jsem třídu Computers V ní se budou ukládat informace o jednotlivých počítačích v evidenci

---
+++ src/cz/engeto/ja/Computers.java
@@ -0,0 +1,40 @@
+package cz.engeto.ja;
+
+import java.math.BigDecimal;
+
+public class Computers {
+
+    private String description;
+    private int yearOfProduction;
+    private double price;
+
+    public Computers(String description, int yearOfProduction, double price) {
+        this.description = description;
+        this.yearOfProduction = yearOfProduction;
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getYearOfProduction() {
+        return yearOfProduction;
+    }
+
+    public void setYearOfProduction(int yearOfProduction) {
+        this.yearOfProduction = yearOfProduction;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
