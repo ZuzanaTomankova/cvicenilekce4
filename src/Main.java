c414ee00e5bd8512a4ea34b70cb20e1202429b77
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-02-14 14:09:26 +0100

    řazení počítačů podle data výroby a podle ceny

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import cz.engeto.ja.Computer;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -8,9 +11,9 @@ public class Main {
 
         List<Computer> computers = new ArrayList<>();
 
-        Computer computer1 = new Computer("Dell",2019,15000);
-        Computer computer2 = new Computer("HP",2018,12000);
-        Computer computer3 = new Computer("Lenovo",2017,10000);
+        Computer computer1 = new Computer("Dell",2019, BigDecimal.valueOf(15000));
+        Computer computer2 = new Computer("HP",2018,BigDecimal.valueOf(12000));
+        Computer computer3 = new Computer("Lenovo",2017,BigDecimal.valueOf(10000));
 
         computers.add(computer1);
         computers.add(computer2);
@@ -19,6 +22,21 @@ public class Main {
         for (Computer computer : computers){
             System.out.println(computer.getDescription()+" "+computer.getYearOfProduction()+" "+computer.getPrice());
         }
+        Collections.sort(computers, Comparator.comparing(Computer::getYearOfProduction)) ;
+        System.out.println("Sorted by year of production: ");
+        computers.forEach(System.out::println);
+
+        //the same
+        //for (Computer computer : computers){
+        // System.out.println(computer);}
+
+        Collections.sort(computers, Comparator.comparing(Computer::getPrice).reversed());
+
+        System.out.println("Sorted by price from lowest:");
+        computers.forEach(System.out::println);
 
     }
+
+
+
 }
